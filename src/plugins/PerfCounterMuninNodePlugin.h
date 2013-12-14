c148f1b349e0bb986c10da1c038caa69cf90f03f
Stefan Seidel <github@stefanseidel.info>
2013-12-14 22:31:39 +0100

    option to use localized object names
    performance counter names are localized in the system locale, this makes
    the .ini files non-portable, so introduce the translation mechanism to
    have the .ini file use English counter names, the localized names are
    then looked up in the registry

--- src/plugins/PerfCounterMuninNodePlugin.h
+++ src/plugins/PerfCounterMuninNodePlugin.h
@@ -15,6 +15,7 @@ public:
   static const char *SectionPrefix;
 private:
   bool OpenCounter();
+  const TCHAR *PerfCounterMuninNodePlugin::GetPdhCounterLocalizedName(const TCHAR *englishName);
 
   bool m_Loaded;
   std::string m_SectionName;
