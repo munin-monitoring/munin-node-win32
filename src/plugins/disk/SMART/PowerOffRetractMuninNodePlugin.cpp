bc1d66957616eec049d3b72f294e770727ef70b2
lemontree23 <lourensros@gmail.com>
2020-01-28 14:43:44 +0200

    Added Plugin to graph Emergency head retracts of Hard Drive

---
+++ src/plugins/disk/SMART/PowerOffRetractMuninNodePlugin.cpp
@@ -0,0 +1,82 @@
+/* This file is part of munin-node-win32
+ * Copyright (C) 2006-2007 Jory Stone (jcsston@jory.info)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "StdAfx.h"
+#include "PowerOffRetractMuninNodePlugin.h"
+
+PowerOffRetractMuninNodePlugin::PowerOffRetractMuninNodePlugin()
+{
+  
+}
+
+PowerOffRetractMuninNodePlugin::~PowerOffRetractMuninNodePlugin()
+{
+
+}
+
+int PowerOffRetractMuninNodePlugin::GetConfig(char *buffer, int len)
+{
+  int ret = 0;
+  int index = 0;
+
+  ret = _snprintf(buffer, len, "graph_title Power-off Retract/Unsafe Shutdown Count\n"
+    "graph_args --base 1000 -l 0\n"
+    "graph_vlabel Amount\n"
+    "graph_category S.M.A.R.T\n"
+    "graph_info This graph shows the Amount of unsafe shutdowns for each drive in the machine.\n");
+  buffer += ret;
+  len -= ret;
+
+  JCAutoLockCritSec lock(&g_SmartReaderCritSec);
+  g_SmartReader.UpdateSMART();
+  for (index = 0; index < g_SmartReader.m_ucDrivesWithInfo; index++) {
+    ret = _snprintf(buffer, len, "_hdd_%i_.label %s\n", index, 
+      (PCHAR)g_SmartReader.m_stDrivesInfo[index].m_stInfo.sModelNumber);
+    len -= ret;
+    buffer += ret;
+  }
+
+  strncat(buffer, ".\n", len);
+
+  return 0;
+}
+
+int PowerOffRetractMuninNodePlugin::GetValues(char *buffer, int len)
+{ 
+  int index = 0;
+  int ret;    
+
+  JCAutoLockCritSec lock(&g_SmartReaderCritSec);
+  g_SmartReader.UpdateSMART();
+  for (index = 0; index < g_SmartReader.m_ucDrivesWithInfo; index++) {
+    ST_DRIVE_INFO *pDriveInfo = g_SmartReader.GetDriveInfo(index);
+    if (!pDriveInfo)
+      continue;
+
+    ST_SMART_INFO *pSmartInfo = g_SmartReader.GetSMARTValue(pDriveInfo->m_ucDriveIndex, SMART_ATTRIB_POWER_LOSE_RETRACT_COUNT);
+    if (!pSmartInfo)
+      continue;
+
+    ret = _snprintf(buffer, len, "_hdd_%i_.value %i\n", index, pSmartInfo->m_dwAttribValue);
+    len -= ret;
+    buffer += ret;
+  }
+
+  strncat(buffer, ".\n", len);
+  return 0;
+}
